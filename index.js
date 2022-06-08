0c5422a2e2f91271771df3d7f6a10dfbc79f7ea8
Gabriel dos Santos Oliveira <86084272+GabrieldosSantosOliveira@users.noreply.github.com>
2022-06-08 19:15:02 +0000

    Arrumado os erros das rotas.

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let express = require("express");
 let {autor, livro} = require("./models");
 let app = express();
 const path = require("path");
-
+let cors = require('cors')
+app.use(cors())
+app.use((req, res, next) => {
+   console.log("Acessou o servidor")
+  res.header("Access-Control-Allow-Origin", "*")
+  app.use(cors());
+  next()
+})
 app.use(express.json());
 
 app.use(express.urlencoded({
@@ -10,24 +17,28 @@ app.use(express.urlencoded({
 }))
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", async function(req,res){
+app.get("/autor", async function(req,res){
   let resultado = await autor.findAll()
   res.json(resultado)
 })
-app.get("/:id", async function(req,res){
+app.get("/autor/:id", async function(req,res){
   let resultado = await autor.findByPk(req.params.id)
   res.json(resultado)
 })
-app.post("/", async function(req,res){
+app.get("/autor/:id/livros", async function(req,res){
+  let resultado = await autor.findByPk(req.params.id, {include:"livros"})
+  res.json(resultado)
+})
+app.post("/autor", async function(req,res){
     console.log(req.body)
   let resultado = await autor.create(req.body)
   res.json(resultado)
 })
-app.put("/:id", async (req,res) =>{
+app.put("/autor/:id", async (req,res) =>{
   let resultado = await autor.update(req.body,{where:req.params})
   res.json(resultado)
 })
-app.delete("/:id", async (req,res) =>{
+app.delete("/autor/:id", async (req,res) =>{
   let resultado = await autor.destroy({where:req.params})
   res.json(resultado)
 })
@@ -36,8 +47,13 @@ app.get("/livros", async (req,res) => {
   res.json(resultado)
 })
 app.get("/livros/:id", async (req,res) => {
-  try{
   let resultado = await livro.findByPk(req.params.id)
+  res.json(resultado)
+    res.json(e)
+})
+app.get("/livros/:id/autor", async (req,res) => {
+  try{
+  let resultado = await livro.findByPk(req.params.id, {include: "autor"})
   res.json(resultado)
   } catch(e){
     res.json(e)
