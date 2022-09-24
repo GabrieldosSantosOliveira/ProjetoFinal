6782d5ce019e398958e97c6939a562d88f4101a3
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-09-24 19:06:56 -0400

    feat: done Added try catch in controllers

--- src/controllers/Autor.js
+++ src/controllers/Autor.js
@@ -2,28 +2,52 @@ let { autor } = require("../../models");
 
 class Autor {
   async FindOne(req, res) {
+    try{
     let resultado = await autor.findByPk(req.params.id)
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async FindAll(req, res) {
+    try{
     let resultado = await autor.findAll()
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async FindRelations(req, res) {
+    try{
     let resultado = await autor.findByPk(req.params.id, { include: "livros" })
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async Create(req, res) {
+    try{
     let resultado = await autor.create(req.body)
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async Update(req, res) {
+    try{
     let resultado = await autor.update(req.body, { where: req.params })
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async Delete(req, res) {
+    try{
     let resultado = await autor.destroy({ where: req.params })
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
 }
 module.exports = { Autor }
--- src/controllers/Livro.js
+++ src/controllers/Livro.js
@@ -2,30 +2,54 @@ let { livro } = require("../../models");
 
 class Livro {
   async FindAll(req, res) {
+    try{
     let resultado = await livro.findAll();
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async FindOne(req, res) {
+    try{
     let resultado = await livro.findByPk(req.params.id)
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async FindRelations(req, res) {
+    try{
     let resultado = await livro.findByPk(req.params.id, { include: "autor" })
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async Create(req, res) {
+    try{
     let resultado = await livro.create(req.body)
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
   async Update(req, res) {
+    try{
     let resultado = await livro.update(req.body, {
       where: req.params
     })
     res.json(resultado)
+  } catch(error){
+    res.json({error})
+  }
   }
   async Delete(req, res) {
+    try{
     let resultado = await livro.destroy({ where: req.params })
     res.json(resultado)
+    } catch(error){
+      res.json({error})
+    }
   }
 }
 
