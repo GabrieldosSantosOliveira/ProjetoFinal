5fa19bcfdcc8f0950b11b1e91c76c683c0cf80ba
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-09-28 08:52:13 -0400

    Update vercel.json and index.js

--- index.js
+++ index.js
@@ -1,62 +1,26 @@
-let express = require("express");
-let {autor, livro} = require("./models");
-let app = express();
+const express = require("express");
+const app = express();
 const path = require("path");
-
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const { routerAutor } = require("./src/routes/autor");
+const { routerLivro } = require("./src/routes/livro");
+require("dotenv").config();
+app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", async function(req,res){
-  let resultado = await autor.findAll()
-  res.json(resultado)
-})
-app.get("/:id", async function(req,res){
-  let resultado = await autor.findByPk(req.params.id)
-  res.json(resultado)
-})
-app.post("/", async function(req,res){
-    console.log(req.body)
-  let resultado = await autor.create(req.body)
-  res.json(resultado)
-})
-app.put("/:id", async (req,res) =>{
-  let resultado = await autor.update(req.body,{where:req.params})
-  res.json(resultado)
-})
-app.delete("/:id", async (req,res) =>{
-  let resultado = await autor.destroy({where:req.params})
-  res.json(resultado)
-})
-app.get("/livros", async (req,res) => {
-  let resultado = await livro.findAll();
-  res.json(resultado)
-})
-app.get("/livros/:id", async (req,res) => {
-  try{
-  let resultado = await livro.findByPk(req.params.id)
-  res.json(resultado)
-  } catch(e){
-    res.json(e)
-  }
-})
-app.post("/livros", async(req, res) =>{
-  let resultado =  await livro.create(req.body)
-  res.json(resultado)
-})
-app.put("/livros/:id", async (req, res) => {
-  let resultado = await livro.update(req.body, {
-    where:req.params
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-  res.json(resultado)
-})
-app.delete("/livros/:id", async(req,res) => {
-  let resultado = await livro.destroy({where:req.params})
-  res.json(resultado)
-})
-app.listen(3500,() => {
-  console.log("Eu estou cansado")
-})
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routerAutor);
+app.use(routerLivro);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
